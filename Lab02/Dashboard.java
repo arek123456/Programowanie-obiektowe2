cbb404c79a71bc937971f6373d09f7656ec7b1d5
Arkadiusz Haznar <128134965+arek123456@users.noreply.github.com>
2023-11-14 18:44:08 +0100

    Add files via upload

---
+++ Lab02/Dashboard.java
@@ -0,0 +1,18 @@
+package Lab02;
+
+import javax.swing.*;
+
+public class Dashboard extends JFrame{
+    private JPanel dashboardPanel;
+    private JButton btnClear;
+    private JButton btnDelete;
+    private JButton btnEdit;
+    private JButton btnRegister;
+    private JPanel JPanel;
+    private JLabel IbAdmin;
+    private JTable ClientsTable;
+    private JButton btnClose;
+    public Dashboard(){
+
+    }
+}
