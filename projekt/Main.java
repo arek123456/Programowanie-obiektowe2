8614e35065f437c16312bc527fa609e2ee3a41e6
Arkadiusz Haznar <128134965+arek123456@users.noreply.github.com>
2023-12-15 15:52:24 +0100

    Add files via upload

---
+++ projekt/Main.java
@@ -0,0 +1,8 @@
+package Projekt;
+
+public class Main {
+    public static void main(String[] args) {
+        Logowanie logowanie = new Logowanie(null);
+        logowanie.setVisible(true);
+    }
+}
---
+++ projekt/Uzytkownik.java
@@ -0,0 +1,9 @@
+package Projekt;
+
+public class Uzytkownik {
+    public String imie;
+    public String nazwisko;
+    public String email;
+    public String login;
+    public String haslo;
+}
